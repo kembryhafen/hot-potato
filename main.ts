67727f3cbab330d8b27299bfc68161b82ab582f2
kembryhafen <102975848+kembryhafen@users.noreply.github.com>
2022-04-13 19:46:40 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,13 +3,19 @@ input.onButtonPressed(Button.A, function () {
         basic.pause(1000)
         basic.showIcon(IconNames.Happy)
         strip.clear()
-        strip.showColor(neopixel.rgb(randint(20, 30), randint(0, 65), randint(0, 20)))
-        strip.showBarGraph(0, count)
+        range5 = strip.range(count - 15, 15)
+        range4 = strip.range(0, count)
+        range5.showColor(neopixel.colors(NeoPixelColors.Black))
+        range4.showColor(neopixel.rgb(randint(0, 65), randint(0, 65), randint(0, 65)))
+        range4.show()
+        range5.show()
         count = count - 1
     }
     if (count == -1) {
-        for (let index = 0; index < 6; index++) {
+        for (let index = 0; index < 10; index++) {
             strip.clear()
+            basic.pause(100)
+            strip.showColor(neopixel.colors(NeoPixelColors.Black))
             basic.pause(100)
             strip.showColor(neopixel.colors(NeoPixelColors.Red))
         }
@@ -57,12 +63,26 @@ input.onButtonPressed(Button.A, function () {
             `)
         basic.pause(1000)
         basic.showString("You Lose")
+        strip.showColor(neopixel.colors(NeoPixelColors.Black))
     }
 })
+let range4: neopixel.Strip = null
+let range5: neopixel.Strip = null
 let count = 0
 let strip: neopixel.Strip = null
 strip = neopixel.create(DigitalPin.P0, 15, NeoPixelMode.RGB)
 count = 10
+let range = strip.range(0, 5)
+let range2 = strip.range(5, 10)
+let range3 = strip.range(10, 15)
+range.showColor(neopixel.rgb(randint(0, 10), randint(10, 20), randint(20, 30)))
+range2.showColor(neopixel.rgb(randint(30, 60), randint(30, 40), randint(40, 60)))
+range3.showColor(neopixel.rgb(randint(20, 30), randint(0, 65), randint(0, 20)))
+for (let index = 0; index < 15; index++) {
+    strip.rotate(1)
+    strip.show()
+    basic.pause(50)
+}
 basic.showString("Hot Potato")
 basic.showLeds(`
     . # . . .
@@ -71,15 +91,13 @@ basic.showLeds(`
     # . . . #
     . # # # .
     `)
-let range = strip.range(0, 5)
-let range2 = strip.range(5, 10)
-let range3 = strip.range(10, 15)
 range.showColor(neopixel.rgb(randint(0, 10), randint(10, 20), randint(20, 30)))
 range2.showColor(neopixel.rgb(randint(30, 60), randint(30, 40), randint(40, 60)))
 range3.showColor(neopixel.rgb(randint(20, 30), randint(0, 65), randint(0, 20)))
 while (!(input.buttonIsPressed(Button.A))) {
     strip.rotate(1)
     strip.show()
+    basic.pause(50)
 }
 basic.forever(function () {
 	
